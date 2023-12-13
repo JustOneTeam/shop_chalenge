929ed607e4392c20a0c6c1a022d8a7ae6a448b33
Dayan <136735481+doocharbex@users.noreply.github.com>
2023-12-13 15:36:28 +0330

    Shop Project
    in this commit  add project, name file project index.html / script.js / Read.me
    
     Linking the css file in the head and using the defer attribute for the html file
    
    using method push (method Array ) to add product

---
+++ JS/script.js
@@ -0,0 +1,52 @@
+/*
+Project courses JS One
+Project Name  : Shop Chalenge
+Teacher Name : Mr.Khateri
+Date : 2023 13 December
+project 2
+
+=========================
+Developers Teams : =>
+
+Dayan Ghanbari
+________________
+Maedeh Khosravii
+________________
+Nazanin Sharifi
+________________
+shayan Mahmoodi
+________________
+Amir Ali Hashemi
+________________
+Omid Bahmanpoor
+________________
+
+========================
+
+
+*/
+
+
+let Shop = {
+    // products Array
+    products: [
+        {
+            name: "Laptop",
+            model: ["Asus ROG J1", "HP W321", "Del", 'Appel Macbook m1 ']
+        },
+        {
+            name: "Mobile",
+            model: ["Samsung", "shiami", "Apple"]
+        },
+        {
+            name: "smartWatch",
+            model: ["Apple watch silver", "Galaxy watch 5pro", "shiami watch Amazfit_GTS"]
+        }
+    ]
+
+}
+
+
+Shop.products.push({name : 'PC' , model : ['Appel'] })
+
+console.log(Shop);
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# shop_chalenge
-Shop Chalenge 
+# sotore_chalenge
+
+****
+![alt text](https://www.blogtyrant.com/wp-content/uploads/2019/07/how-to-start-an-online-store-min.png)
+****
+# Team Development :
+
+<span style="color:#1c49d1">**_Dayan_** Ghanbari</span>
+
+
+<span style="color:#e575a5">**Maedeh** Khosravii</span>
+
+<span style="color:#ff6d38">**Nazanin** Sharifi</span>
+
+<span style="color:#77c5d1">**shayan** Mahmoodi</span>
+
+<span style="color:#759913">**Amir Ali** Hashemi</span>
+
+<span style="color:#f6f98e">**Omid** Bahmanpoor</span>
+****
+
+
